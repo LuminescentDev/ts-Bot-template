20cb510106a2f22c41c3308bea832ad8a70ba7c0
AkiraDev <akira.devjs@gmail.com>
2023-05-17 09:36:47 -0700

    fix Commands

--- src/commands/default/test.ts
+++ src/commands/default/test.ts
@@ -3,7 +3,8 @@ import { Command } from '~/types/Objects';
 export const test: Command = {
 	description: "test command!",
     options: require('~/options/test').default,
-	execute: function (interaction, args, client) {
-		throw new Error('Function not implemented.');
+	execute: function (interaction, args) {
+		const option = args.getString("option");
+		interaction.editReply({content: `You chose ${option}`})
 	}
 }
